77659746dc7ca1a3170da77f357db5f0d80e7457
Imam Susanto <abc.imams@gmail.com>
2026-01-01 08:26:48 +0700

    fix: ImgProxy helper creates new instance

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,6 @@ use Imsus\ImgProxy\ImgProxy;
 if (! function_exists('imgproxy')) {
     function imgproxy(string $url): ImgProxy
     {
-        return app(ImgProxy::class)->url($url);
+        return (new ImgProxy)->url($url);
     }
 }
